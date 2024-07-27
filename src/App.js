e128dd0463f105cf551ab2c2ad6d14d494326255
sven <sven.song@qq.com>
2024-07-27 14:09:51 +0800

    P52-25:46

---
+++ src/App.js
@@ -0,0 +1,9 @@
+
+import React,{Component} from "react";
+import Hello from "./components/Hello/Hello";
+export default class App extends Component{
+    render(){
+        return(<Hello></Hello>)
+    }
+}
+
