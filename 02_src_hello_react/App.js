4fda76d0e31ee0725c5f1ea3a6fb1ee341265d06
sven <sven.song@qq.com>
2024-07-27 15:37:26 +0800

    P55 6：54

--- 02_src_hello_react/App.js
+++ 02_src_hello_react/App.js
@@ -1,11 +1,14 @@
 
 import React,{Component} from "react";
-
+import Hello from "./components/Hello/Hello";
+import Welcom from "./components/Welcom";
 export default class App extends Component{
     render(){
         return(
         <div>
-        hhhh
+        <Hello></Hello>
+
+            <Welcom/>
             </div>
         )
     }
