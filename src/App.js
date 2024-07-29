28b5a73a121247b25b3e93227caefdea2f547b06
sven <sven.song@qq.com>
2024-07-29 09:44:39 +0800

    P56 optimize

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 
-import React,{Component} from "react";
+import React, { Component } from "react";
 
 import Header from './components/Header'
 import Footer from "./components/Footer";
 import List from "./components/List";
 import './App.css'
-export default class App extends Component{
-    render(){
-        return(
-        <div>
-            <Header></Header>
-            <List></List>
-            <Footer></Footer>
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <div class="todo-container">
+                    <div class="todo-wrap">
+                        <Header></Header>
+                        <List></List>
+                        <Footer></Footer>
+                    </div>
+                </div>
             </div>
         )
     }
