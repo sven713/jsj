b3b1ea12a3da0d72bad240adbd8c59f492c02202
sven <sven.song@qq.com>
2024-08-24 21:43:06 +0800

    P58 OK

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,31 @@
 import React, { Component } from 'react'
+
+import {nanoid} from 'nanoid'
+
 import './index.css'
+
+// console.log(nanoid())
+
 export default class Header extends Component {
 
   // handleKeyUp(e){ 不行,赋值语句+箭头函数
   handleKeyUp=(e)=>{
     const {target} = e
     console.log('2222',target.value)
-    if(e.code === "Enter"){
-      //TODO: 去掉handleChange  在这里就能取得已经输入的值
+    if(e.code === "Enter" && target.value.trim()=== ''){
+      alert('输入不能为空')
+      return
+    }
+    if(e.code === "Enter" && target.value){
+      // 去掉handleChange  在这里就能取得已经输入的值
       const todo = {
         name:target.value,
         done:false,
-        id : '90'
+        id : nanoid()
       }
       this.props.addTodos(todo)
-      // this.props.a(target.value)
+
+      target.value = ''
     }
   }
 
