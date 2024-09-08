be73b746524cbd7fca86e0cf0db0fceae196c5bc
sven <sven.song@qq.com>
2024-09-08 15:44:56 +0800

    P65 pxoxy in jsj

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 
 import React, { Component } from "react";
 
+import axios from "axios";
 
 export default class App extends Component {
 
+    getStudentData =()=>{
+        console.log(111111)
+
+        // 前端3000    后端5000
+    
+        axios.get('http://localhost:3000/index1.html').then(
+            response => {
+                console.log('成功了',response.data)
+            },
+            error =>{
+                console.log('失败了',error )
+            }
+        )
+    }
 
     render() {
         return (
             <div>
-                app...
+                <button onClick={this.getStudentData}>点我获取学生数据</button>
             </div>
         )
     }
