9be826ebcbc0cdee4e554e358fe5c8c84b4afca7
sven <sven.song@qq.com>
2024-09-08 22:18:01 +0800

    备份todo:67

---
+++ 04_代理_没成功/App.js
@@ -0,0 +1,43 @@
+
+import React, { Component } from "react";
+
+import axios from "axios";
+
+export default class App extends Component {
+
+    getStudentData =()=>{
+        console.log(111111)
+
+        // 前端3000    后端5000
+    
+        axios.get('http://localhost:3000/api1/students').then(
+            response => {
+                console.log('成功了',response.data)
+            },
+            error =>{
+                console.log('失败了',error )
+            }
+        )
+    }
+
+    getCarData =()=> {
+        axios.get('http://localhost:3000/cars').then(
+            response => {
+                console.log('成功了',response.data)
+            },
+            error =>{
+                console.log('失败了',error )
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.getStudentData}>点我获取学生数据</button>
+                <button onClick={this.getCarData}>点我获取汽车数据</button>
+            </div>
+        )
+    }
+}
+
