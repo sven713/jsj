b9e3c50254aa2f52e39d970de56c557bba818508
sven <sven.song@qq.com>
2024-09-09 22:36:19 +0800

    P68请求

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,14 +1,45 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
 export default class Search extends Component {
+
+
+  state = {
+    inputStr:''
+  }
+
+  search = ()=>{
+    // 获取input值
+    const { inputStr } = this.state
+
+    console.log('!!!!',inputStr)
+    // https://api.github.com/search/users?q=xxxxxx
+    axios.get(`https://api.github.com/search/users?q=${inputStr}`).then(
+      response => {
+        console.log('成功了',response.data)
+      },
+      error =>{
+          console.log('失败了',error )
+      }
+    )
+
+  }
+
+  handleInput = (e)=> {
+    
+    this.setState({
+      inputStr:e.target.value
+    })
+  }
+
   render() {
     return (
       <section className="jumbotron">
-          <h3 className="jumbotron-heading">Search Github Users</h3>
-          <div>
-            <input type="text" placeholder="enter the name you search"/>&nbsp;<button>Search</button>
-          </div>
-        </section>
+        <h3 className="jumbotron-heading">Search Github Users</h3>
+        <div>
+          <input onChange={this.handleInput} type="text" placeholder="enter the name you search" />&nbsp;
+          <button onClick={this.search}>Search</button>
+        </div>
+      </section>
     )
   }
 }
