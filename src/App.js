5d3b69b1c86a8e5418a0987b7946455f53a64034
sven <sven.song@qq.com>
2024-09-21 20:50:53 +0800

    P82,样式丢失解法1，去掉点20：21

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {Route,Switch} from 'react-router-dom'
 import About from './pages/About'
 import Home from './pages/Home'
 import Header from './components/Header'
-import Test from './pages/Test'
 import MyNavLink from './components/MyNavLink/MyNavLink'
 
 export default class gitDemo extends Component {
@@ -25,19 +24,17 @@ export default class gitDemo extends Component {
           <div className="col-xs-2 col-xs-offset-2">
             <div className="list-group">
               
-                {/* <NavLink activeClassName='aClass' className='list-group-item' to='/about'>about</NavLink>
-                <NavLink activeClassName='aClass' className='list-group-item' to='/home'>home</NavLink> */}
-                <MyNavLink to='/about' title='about' children='about!'/>
-                <MyNavLink to='/home' title='home' children='home!!'/>
+               
+                <MyNavLink to='/at/about' title='about' children='about!'/>
+                <MyNavLink to='/at/home' title='home' children='home!!'/>
             </div>
           </div>
           <div className="col-xs-6">
             <div className="panel">
               <div className="panel-body">
                 <Switch>
-                <Route path='/about' component={About}></Route>
-                <Route path='/home' component={Home}></Route>
-                <Route path='/home' component={Test}></Route>
+                <Route path='/at/about' component={About}></Route>
+                <Route path='/at/home' component={Home}></Route>
                 </Switch>
               </div>
             </div>
