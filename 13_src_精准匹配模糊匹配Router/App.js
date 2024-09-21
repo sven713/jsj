8d40403d48dc80c4b41a65715f7ad5ccca6a0f84
sven <sven.song@qq.com>
2024-09-21 21:44:15 +0800

    P84准备

---
+++ 13_src_精准匹配模糊匹配Router/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import {Route,Switch} from 'react-router-dom'
+
+import About from './pages/About'
+import Home from './pages/Home'
+import Header from './components/Header'
+import MyNavLink from './components/MyNavLink/MyNavLink'
+
+export default class gitDemo extends Component {
+  
+
+  render() {
+
+    return (
+      <div id="root">
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            
+            <Header></Header>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              
+               
+                <MyNavLink to='/about' title='about' children='about!'/>
+                <MyNavLink to='/home/a/b' title='home' children='home!!'/>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                <Switch>
+                  <Route  path='/about' component={About}></Route>
+                  <Route exact path='/home' component={Home}></Route>
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+  }
+}
