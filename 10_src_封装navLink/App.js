df9d5d7ed4ead5380074eb69b85322884f33645e
sven <sven.song@qq.com>
2024-09-21 19:45:13 +0800

    P81准备

---
+++ 10_src_封装navLink/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import {NavLink ,Route} from 'react-router-dom'
+
+import About from './pages/About'
+import Home from './pages/Home'
+import Header from './components/Header'
+import MyNavLink from './components/MyNavLink/MyNavLink'
+
+export default class gitDemo extends Component {
+  
+
+  render() {
+
+    return (
+      <div id="root">
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            
+            <Header></Header>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              
+                {/* <NavLink activeClassName='aClass' className='list-group-item' to='/about'>about</NavLink>
+                <NavLink activeClassName='aClass' className='list-group-item' to='/home'>home</NavLink> */}
+                <MyNavLink to='/about' title='about' children='about!'/>
+                <MyNavLink to='/home' title='home' children='home!!'/>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                <Route path='/about' component={About}></Route>
+                <Route path='/home' component={Home}></Route>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+  }
+}
