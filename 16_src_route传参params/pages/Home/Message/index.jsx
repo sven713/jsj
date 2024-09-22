31870a11b892f58a8943d5e10e27307fea110d1d
sven <sven.song@qq.com>
2024-09-22 20:49:25 +0800

    P87准备

---
+++ 16_src_route传参params/pages/Home/Message/index.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import {Link, Route} from 'react-router-dom'
+
+import Detail from './Detail'
+
+export default class Message extends Component {
+
+
+  state = {
+    msgArr:[
+      {
+        id:'01',
+        title:'消息1'
+      },
+      {
+        id:'02',
+        title:'消息2'
+      },
+      {
+        id:'03',
+        title:'消息3'
+      },
+    ]
+  }
+
+
+  render() {
+    const {msgArr} = this.state
+    return (
+      <div>
+        <ul>
+         
+          {
+            msgArr.map((msg)=> {
+              return (
+                <li key={msg.id}>
+                  {/* <a href="/message/3">{msg.title}</a> */}
+                  <Link to={`/home/message/detail/${msg.id}/${msg.title}`}>{msg.title}</Link>
+                </li>
+              )
+            })
+          }
+        </ul>
+        <Route path='/home/message/detail/:id/:title2' component={Detail}></Route>
+
+        {/* <Detail></Detail> */}
+      </div>
+    )
+  }
+}
