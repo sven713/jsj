d1acb2d3dac74fdb0d9d2bc492af60e741f27d75
sven <sven.song@qq.com>
2024-09-22 16:54:32 +0800

    P86,12：37

---
+++ src/pages/Home/Message/Detail/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Detail extends Component {
+  render() {
+    return (
+      <div>
+        <li>ID:??</li>
+        <li>Title:??</li>
+        <li>Content:??</li>
+      </div>
+    )
+  }
+}
--- src/pages/Home/Message/index.jsx
+++ src/pages/Home/Message/index.jsx
@@ -1,20 +1,49 @@
 import React, { Component } from 'react'
+import {Link, Route} from 'react-router-dom'
+
+import Detail from './Detail'
 
 export default class Message extends Component {
+
+
+  state = {
+    msgArr:[
+      {
+        id:'01',
+        title:'消息1'
+      },
+      {
+        id:'02',
+        title:'消息2'
+      },
+      {
+        id:'03',
+        title:'消息3'
+      },
+    ]
+  }
+
+
   render() {
+    const {msgArr} = this.state
     return (
       <div>
         <ul>
-          <li>
-            <a href="/message1">message001</a>&nbsp;&nbsp;
-          </li>
-          <li>
-            <a href="/message2">message002</a>&nbsp;&nbsp;
-          </li>
-          <li>
-            <a href="/message/3">message003</a>&nbsp;&nbsp;
-          </li>
+         
+          {
+            msgArr.map((msg)=> {
+              return (
+                <li>
+                  {/* <a href="/message/3">{msg.title}</a> */}
+                  <Link to='/home/message/detail'>{msg.title}</Link>
+                </li>
+              )
+            })
+          }
         </ul>
+        <Route path='/home/message/detail' component={Detail}></Route>
+
+        {/* <Detail></Detail> */}
       </div>
     )
   }
